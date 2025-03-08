ed43d911549e8da52bb15d0af0a6418102bebe8a
Wu Yong Qi <w18178865609@outlook.com>
2025-03-08 21:52:58 +0800

    test

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -15,4 +15,6 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+
+  base: process.env.NODE_ENV === 'production' ? '/Build-your-personal-webpage-with-Vue3-Vite-element-plus-vuetify/' : '/',
 })
